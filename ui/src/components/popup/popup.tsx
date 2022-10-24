2926a366c7a2bd2fe4893d692ea0173c3cb3be47
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-10-24 21:31:42 +0400

    popyp type add

--- ui/src/components/popup/popup.tsx
+++ ui/src/components/popup/popup.tsx
@@ -1,4 +1,36 @@
+import { changeVisiblePopup } from "../../redux/store/popup"
+import { useDispatch, useSelector } from "react-redux"
+import { StoreData } from "../../redux/interface"
+import { useEffect } from "react"
+import './popup.scss'
+
 export const Popup = () => {
 
-    return <div>popup</div>
+    const dispatch = useDispatch()
+    const popupState = useSelector((store: StoreData) => store.visiblePopup)
+
+    useEffect(() => {
+
+    }, [popupState])
+
+    const callbackHandler = () => {
+        popupState.callback()
+        close()
+    }
+
+    const close = () => dispatch(changeVisiblePopup({
+        open: popupState.open, 
+        callback: () => {}, 
+        message: ''
+    }))
+
+    return <div className="popup-container">
+        <section className="popup">
+            <p className="room__message_name">{popupState.message}</p>
+            <div className="button-container button-container_column">
+                <button className="button-create" onClick={callbackHandler}>Да</button>
+                <button className="button-create" onClick={close}>Отмена</button>
+            </div>
+        </section>
+    </div>
 }
