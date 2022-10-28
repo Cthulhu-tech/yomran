9dc6b2b93e569423686907046075a5bd1b9e9a22
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-10-28 04:34:58 +0400

    update store

--- ui/src/redux/store/aside.tsx
+++ ui/src/redux/store/aside.tsx
@@ -8,8 +8,8 @@ const updateVisible = "update_state_visible"
 
 export const visibleAside = (state = defaultState, action:Action<string, boolean>) => {
     switch (action.type){
-        case updateVisible: 
-            return {open: state.open}
+        case updateVisible:
+            return {open: action.payload}
         default:
             return {open: state.open}
     }
