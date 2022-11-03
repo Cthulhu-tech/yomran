5f24de582ec0777f9f1bfa648216e0739bc05082
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-11-03 23:32:20 +0400

    input add

--- ui/src/components/input/input.tsx
+++ ui/src/components/input/input.tsx
@@ -1,26 +1,4 @@
-import { useState, useEffect } from 'react'
-import { Image } from '../image/image'
-import './input.scss'
+export const Input = () => {
 
-import onvisible from '../../assets/onvisible.svg'
-import visible from '../../assets/visible.svg'
-import { ImageType } from '../image/type'
-
-export const InputPassword = () => {
-
-    const [show, setShow] = useState(false)
-    const [imgData, setImageData] = useState<ImageType>({width: '24px', alt: 'show', src: onvisible})
-
-    useEffect(() => {setImageData({width: '24px', alt: 'show', src: show ? visible : onvisible})}, [show])
-
-    const showHandler = () => setShow(!show)
-
-    return <div className='password-container'>
-        <div className='input-container'>
-            <input type={show ? "password" : "text"} className="input input-text"/>
-            <div onClick={showHandler}>
-                <Image {...imgData}/>
-            </div>
-        </div>
-    </div>
+    return <input type="text" className="input input-text"/>
 }
---
+++ ui/src/components/input/inputPassword.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+import { Image } from '../image/image'
+import './input.scss'
+
+import onvisible from '../../assets/onvisible.svg'
+import visible from '../../assets/visible.svg'
+import { ImageType } from '../image/type'
+
+export const InputPassword = () => {
+
+    const [show, setShow] = useState(false)
+    const [imgData, setImageData] = useState<ImageType>({width: '24px', alt: 'show', src: onvisible})
+
+    useEffect(() => {setImageData({width: '24px', alt: 'show', src: show ? visible : onvisible})}, [show])
+
+    const showHandler = () => setShow(!show)
+
+    return  <div className='input-container'>
+                <input type={show ? "password" : "text"} className="input input-text"/>
+                <div onClick={showHandler}>
+                    <Image {...imgData}/>
+                </div>
+        </div>
+}
