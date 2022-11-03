53dd635b14a519f2ca9c052ed21183009d13a3fb
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-11-03 23:19:25 +0400

    update

--- ui/src/components/image/image.tsx
+++ ui/src/components/image/image.tsx
@@ -2,7 +2,7 @@ import { ImageType } from "./type"
 
 export const Image = (type: ImageType) => {
 
-    return <div className='image-wrapper' style={{maxWidth: type.width, minWidth: type.width, height: type.width}}>
+    return <div className={type.src ? 'image-wrapper' : 'image-background'} style={{maxWidth: type.width, minWidth: type.width, height: type.width}}>
         {type.src && <img className={'image ' + type.className} src={type.src} alt={type.alt} style={{maxWidth: type.width, minWidth: type.width, height: type.width}} />}
     </div>
 }
