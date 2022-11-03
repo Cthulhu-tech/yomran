7676a7dc232dd86d8c7c5dc23ec04afc2b4c5219
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-11-03 23:32:35 +0400

    update scss

--- ui/src/components/create/create.tsx
+++ ui/src/components/create/create.tsx
@@ -1,4 +1,5 @@
-import { InputPassword } from '../input/input'
+import { InputPassword } from '../input/inputPassword'
+import { Input } from '../input/input'
 import './create.scss'
 
 export const Create = () => {
@@ -6,7 +7,10 @@ export const Create = () => {
     const handlerSubmit = (event: React.FormEvent<HTMLFormElement>) => event?.preventDefault()
 
     return  <form onSubmit={handlerSubmit} className="form-container">
-        <InputPassword/>
+        <div className='input-container input-flex'>
+            <Input />
+            <InputPassword/>
+        </div>
         <input className='button-create' type="submit" value={'cоздать'}/>
     </form>
 }
