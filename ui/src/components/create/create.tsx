479421b14dd548ed12fb722fe3bdb62bfc72354c
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-11-04 22:33:24 +0400

    create logic update

--- ui/src/components/create/create.tsx
+++ ui/src/components/create/create.tsx
@@ -1,16 +1,33 @@
+import { changeVisibleCreate } from '../../redux/store/create'
 import { InputPassword } from '../input/inputPassword'
+import { useWindow } from '../../hook/useWindow'
+import { useDispatch } from 'react-redux'
 import { Input } from '../input/input'
 import './create.scss'
 
 export const Create = () => {
 
+    const wide = useWindow(760)
+    const dispatch = useDispatch()
     const handlerSubmit = (event: React.FormEvent<HTMLFormElement>) => event?.preventDefault()
 
-    return  <form onSubmit={handlerSubmit} className="form-container">
+    const closeRoom = () => {
+        dispatch(changeVisibleCreate({
+            open: false,
+            callback: () => {}
+        }))
+    }
+
+    return  <form onSubmit={handlerSubmit} className={wide ? "form-container shadow_right" : "form-container"}>
+            <div className='close-wrapper'>
+                <div className='close-button animation-background_medium' onClick={closeRoom}>
+                    <div className='burger-container__button open'></div>
+                </div>
+            </div>
         <div className='input-container input-flex'>
             <Input />
             <InputPassword/>
         </div>
-        <input className='button-create' type="submit" value={'cоздать'}/>
+        <input className='button-create' type="submit" value="создать"/>
     </form>
 }
